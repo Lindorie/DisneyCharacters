cb41672d5ed12775b1b017bb1dcf754259f32b3d
Carine Pic <pic.carine@gmail.com>
2015-10-31 17:56:23 +0000

    update function query_db

--- index.py
+++ index.py
@@ -56,8 +56,8 @@ def init_db():
 
 def query_db(query, args=(), one=False):
   cur = g.db.execute(query, args)
-  rv = cur.fetchall()
-  cur.close()
+  rv = [dict((cur.description[idx][0], value)
+          for idx, value in enumerate(row)) for row in cur.fetchall()]
   return (rv[0] if rv else None) if one else rv
 
 @app.before_request
